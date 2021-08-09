64df8402dede01501271622cadce6e6c3569641a
Victorcreed07 <79251777+Victorcreed07@users.noreply.github.com>
2021-08-09 21:24:29 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,32 +1,74 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=NIWE;port=3306",'root','' );
-if(isset($_GET['name2']))
+session_start();
+$pdo = new PDO("mysql:host=localhost;dbname=NIWE;port=3306",'root','root' );
+if(!empty($_GET['name2']))
 {
 $sql="insert into register(NIWE_Reg_No,Station_name,village,district,state,commision_date,Client,latitude,longitude,mast_height,amount,date_receipt,ddno,followup,survey_map,comment)values(:reg,:stat,:vil,:dist,:state,:comdate,:client,:lat,:long,:mast,:amt,:date,:ddno,:follow,:survey,:comment)";
 
 $stmt=$pdo->prepare($sql);
-$unique= uniqid();
-if(isset($_GET['lat'])&& isset($_GET['long']))
+$num = "true";
+while($num != "false")
 {
-	$dd1=$_GET['lat'];
-	$dd2=$_GET['long'];
+$mad = 'C-WET-PM-R-';
+$mad1=rand(1,100);
+$mad2=strval($mad1);
+$mad3=$mad.$mad2;
+$stmt1=$pdo->prepare("select * from register where NIWE_Reg_No=:reg1");
+$stmt1->execute([':reg1' => $mad3]); 
+$row4=$stmt->fetch(PDO::FETCH_ASSOC);
+if($row4 === false)
+{
+	$num="false";
+	
+	
 }
-if(isset($_GET['deg1'])&&isset($_GET['min1'])&& isset($_GET['sec1']))
+else
+{
+	$num = "true";
+}
+
+
+}
+
+if(!empty($_GET['lat'])&& !empty($_GET['long']))
+{
+	$lat=$_GET['lat'];
+	$log=$_GET['long'];
+}
+if(!empty($_GET['deg1'])&&!empty($_GET['min1'])&& !empty($_GET['sec1']))
 {
 $var1=$_GET['deg1'];
 $var2=$_GET['min1'];
 $var3=$_GET['sec1'];
-$dd1= $var1 + ($var2/60) + ($var3/3600);
+$lat= $var1 + ($var2/60) + ($var3/3600);
 }
-if(isset($_GET['deg2'])&&isset($_GET['min2'])&& isset($_GET['sec2']))
+if(!empty($_GET['deg2'])&&!empty($_GET['min2'])&& !empty($_GET['sec2']))
 {
 $var1=$_GET['deg2'];
 $var2=$_GET['min2'];
 $var3=$_GET['sec2'];
-$dd2= $var1 + ($var2/60) + ($var3/3600);
+$log= $var1 + ($var2/60) + ($var3/3600);
+}
+if(!empty($_GET['easting'])&& !empty($_GET['norting'])&& !empty($_GET['zone']))
+{
+
+
+
+
+$_SESSION["x"] = $_GET['easting'];
+$_SESSION["y"] = $_GET['norting'];
+$_SESSION["z"] = $_GET['zone'];
+include 'utm.php';
+
+$lat=$_SESSION['latitude'];
+$log=$_SESSION['longitude'];
+	
+	
+
+
 }
 
- $stmt->execute(array(':reg' =>$unique ,':stat' =>$_GET['name2'] ,':vil'=>$_GET['village'],':dist' =>$_GET['dist'],':state' =>$_GET['state'],':comdate' =>$_GET['date'],':state' =>$_GET['state'],':client' =>$_GET['owner'],':lat' =>$dd1,':long' =>$dd2,':mast' =>$_GET['mast'],':amt' =>$_GET['amt'],':date' =>$_GET['receipt'],':ddno' =>$_GET['dd'],':follow' =>$_GET['follow'],':survey' =>$_GET['survey'],':comment' =>$_GET['comment'] ));
+ $stmt->execute(array(':reg' =>$mad3 ,':stat' =>$_GET['name2'] ,':vil'=>$_GET['village'],':dist' =>$_GET['dist'],':state' =>$_GET['state'],':comdate' =>$_GET['date'],':state' =>$_GET['state'],':client' =>$_GET['owner'],':lat' =>$lat,':long' =>$log,':mast' =>$_GET['mast'],':amt' =>$_GET['amt'],':date' =>$_GET['receipt'],':ddno' =>$_GET['dd'],':follow' =>$_GET['follow'],':survey' =>$_GET['survey'],':comment' =>$_GET['comment'] ));
 }
  
 ?>
@@ -38,10 +80,12 @@ $dd2= $var1 + ($var2/60) + ($var3/3600);
 	<title>Register</title>
 
 	<style type="text/css">
-		body{
-			margin: 0px;
-			padding: 0px;
-		}
+		#header{
+			background-image:repeating-radial-gradient(rgb(95, 95, 255),white);
+    		padding: 20px 20px 20px 20px ;
+    		text-align: center;
+
+			}
 		#deg1
 		{
 			width: 20px;
@@ -106,56 +150,16 @@ $dd2= $var1 + ($var2/60) + ($var3/3600);
 			padding: 30px;
 			margin-left: 20px;
 		}
-		#dms
-		{
-			display: none;
-		}
-		#dd1
-		{
-			display: none;
-  			overflow: hidden;
-		}
-		
-		.topnav {
-			overflow: hidden;
-			background-color: rgb(0, 191, 255);
-		}
-		
-		.topnav a {
-			float: left;
-			color: #000000;
-			text-align: center;
-			padding: 14px 16px;
-			text-decoration: none;
-			font-size: 17px;
-			align-content: center;
-		}
-		.topnav a:hover {
-			background-color: #ddd;
-			color: black;
-		}
-		
-		.topnav a.active {
-			background-color: #4CAF50;
-			color: white;
-		}
+
 	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-	<div id ="header">
-	 	<?php include "header.php" ?>
-    </div>
-	<div class="topnav">    
-                
-                <a href="home.php">Home</a>
-                <a href="register.php" class="active">Register</a>
-                <a href="view.php">Database</a>
-                <a>ReportGeneration</a>
-                <a href="settings.php">Settings</a>
-                <a>Export</a>
-                <a href="logout.php">Logout</a>
-    </div>
-	<br>
+	 <div id ="header">
+                <h1>MAST REGISTRATION PORTAL</h1>
+                <h3>National Institute of Wind Energy</h3>
+                <h4>(Ministry of New and Renewable Energy)</h4>
+            </div>
 	<div id="form">
 	<form method="GET">
 		<h2> Registeration form</h2>
@@ -166,9 +170,9 @@ $dd2= $var1 + ($var2/60) + ($var3/3600);
 			<div id="radio">
 			<p>DMS<input type="radio" name="coord" value="DMS" id="dms1" onclick="dms();"></p>
 			<p>&emsp;DD<input type="radio" name="coord" value="DD" id="dd" onclick="dd5();"></p>
-			<p>&emsp;UTM<input type="radio" name="coord" value="UTM" id="utm"></p>
+			<p>&emsp;UTM<input type="radio" name="coord" value="UTM" id="utm" onclick="utm();"></p>
 		</div>
-			<div id="dms">
+			<div id="dms" style="display: none">
 				<span>Latitude</span>
 				<p>Deg<input type="integer" name="deg1" id="deg1">Min<input type="integer" name="min1" id="min1">Sec<input type="integer" name="sec1" id="sec1">
 				<br>
@@ -177,8 +181,14 @@ $dd2= $var1 + ($var2/60) + ($var3/3600);
 				<p>Deg<input type="integer" name="deg2" id="deg2">Min<input type="integer" name="min2" id="min2">Sec<input type="integer" name="sec2" id="sec2">
 				
 			</div>
-			<div id="dd1">
+			<div id="dd1" style="display: none">
 			<p>Latitude:<input type="text" name="lat" id="lat1"><br>Longitude:<input type="text" name="long" id="lat1"></p>
+		</div>
+		<div id="utm1" style="display: none">
+			<p>UTM Easting<input type="text" name="easting" id="easting" size="5"></p>
+			<p>UTM Northing<input type="text" name="norting" id="norting" size="5"></p>
+			<p>Zone<input type="integer" name="zone" id="zone" size="5">
+
 		</div>
 			
 </fieldset>
@@ -207,20 +217,34 @@ $dd2= $var1 + ($var2/60) + ($var3/3600);
 
 	</form>
 	<script type="text/javascript">
+
 		function display()
 		{
 			textarea.style.display="block";
 		}
-		function dms()
-		{
-			document.getElementById("dms").style.display = "block";
-			document.getElementById("dd1").style.display = "none";
-		}
-		function dd5()
-		{
-			document.getElementById("dd1").style.display = "block";
-			document.getElementById("dms").style.display = "none";
-		}
+
+		 $(function () {
+        $("input[name='coord']").click(function () {
+            if ($("#dms1").is(":checked")) {
+                $("#dms").show();
+            } else {
+                $("#dms").hide();
+            }
+
+            if ($("#dd").is(":checked")) {
+                $("#dd1").show();
+            } else {
+                $("#dd1").hide();
+            }
+
+
+            if ($("#utm").is(":checked")) {
+                $("#utm1").show();
+            } else {
+                $("#utm1").hide();
+            }
+        });
+    });
 		
 	</script>
 
