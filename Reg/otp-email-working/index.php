86eb178a162698172205dd4243195ba5605d4d40
Victorcreed07 <79251777+Victorcreed07@users.noreply.github.com>
2021-09-05 13:27:20 +0530

    Update index.php

--- Reg/otp-email-working/index.php
+++ Reg/otp-email-working/index.php
@@ -9,7 +9,7 @@ $result = mysqli_query($conn,"SELECT * FROM users WHERE email='" . $_POST["email
 	$count  = mysqli_num_rows($result);
 	if($count>0) {
 		// generate OTP
-		$otp = rand(100000,999999);
+		$otp = rand(100000,999999); //generating a random otp
 		// Send OTP
 		require_once("mail_function.php");
 		$mail_status = sendOTP($_POST["email"],$otp);
@@ -17,7 +17,7 @@ $result = mysqli_query($conn,"SELECT * FROM users WHERE email='" . $_POST["email
 		if($mail_status == 1) 
 		{
 			$result = mysqli_query($conn,"INSERT INTO otpstore(otp,is_expired,create_at) VALUES ('" .$otp. "',0, '" .date("Y-m-d H:i:s"). "')");
-			$current_id = mysqli_insert_id($conn);
+			$current_id = mysqli_insert_id($conn); //storing the timestamp and otp
 			
 				
 				$success=1;
